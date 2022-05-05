512d8746a3a32a95aab6eef6b22c8923ef249f04
Cindy Vuong <vuongcindytv@gmail.com>
2022-05-04 23:40:24 -0700

    passes all test cases

--- W3D3/recursion_hw.rb
+++ W3D3/recursion_hw.rb
@@ -6,15 +6,13 @@ def sum_to(n) #doesn't work
     #inductive step
     if n > 0
         n + sum_to(n - 1)
-    else
-        nil
     end
 end
 
-# p sum_to(5)  # => returns 15
-# p sum_to(1)  # => returns 1
-# p sum_to(9)  # => returns 45
-# p sum_to(-8)  # => returns nil
+p sum_to(5)  # => returns 15
+p sum_to(1)  # => returns 1
+p sum_to(9)  # => returns 45
+p sum_to(-8)  # => returns nil
 
 # exercise 2
 def add_numbers(nums_arr) #incomplete
