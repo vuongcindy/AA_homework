1b146bea6dd65fd2a46264f7d801b5b4817a1993
Cindy Vuong <vuongcindytv@gmail.com>
2022-05-16 11:34:36 -0700

    passes example tests

---
+++ W5D1/LRU_cache.rb
@@ -0,0 +1,52 @@
+class LRUCache
+    def initialize(max_length)
+      @max_length = max_length
+      @cache = []
+    end
+
+    def count
+      # returns number of elements currently in cache
+      @cache.length
+    end
+
+    def add(el)
+      # adds element to cache according to LRU principle
+      if !@cache.include?(el) && self.count + 1 <= @max_length
+        @cache.push(el)
+      elsif !@cache.include?(el) 
+        @cache.push(el)
+        @cache.shift
+      else # accessing an ele
+        @cache.delete(el)
+        @cache.push(el)
+      end
+    end
+
+    def show
+      # shows the items in the cache, with the LRU item first
+      @cache
+    end
+
+    private
+    # helper methods go here!
+
+  end
+
+  #test
+  johnny_cache = LRUCache.new(4)
+
+  johnny_cache.add("I walk the line")
+  johnny_cache.add(5)
+  johnny_cache.count # => returns 2
+  johnny_cache.add([1,2,3])
+  johnny_cache.add(5)
+  johnny_cache.add(-5)
+  johnny_cache.add({a: 1, b: 2, c: 3})
+  johnny_cache.add([1,2,3,4])
+  johnny_cache.add("I walk the line")
+  johnny_cache.add(:ring_of_fire)
+  johnny_cache.add("I walk the line")  
+  johnny_cache.add({a: 1, b: 2, c: 3})
+
+
+  p johnny_cache.show # => prints [[1, 2, 3, 4], :ring_of_fire, "I walk the line", {:a=>1, :b=>2, :c=>3}]
